c56c4cfc496b08e26eff808d67dcfeb895ce8b00
yashk28 <61261792+yashk28@users.noreply.github.com>
2023-08-05 23:34:36 +0530

    add btn added (#1)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Navbar from './components/Navbar';
-
+import AddBtn from './components/AddBtn';
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <div className='container'>
+        <AddBtn/>
+      </div>
     </div>
   );
 }
